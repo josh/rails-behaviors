875e74d8db6f5fcdf1da233da52c0a4ae7fbac93
Joshua Peek <josh@joshpeek.com>
2012-09-17 17:32:43 -0500

    Just require coffee-script

--- accept.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-  $(document).on('ajaxBeforeSend', function(event, xhr, settings) {
-    if (!settings.dataType) {
-      return xhr.setRequestHeader('Accept', '*/*;q=0.5, ' + settings.accepts.script);
-    }
-  });
-
-}).call(this);
